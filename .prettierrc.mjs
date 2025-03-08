dc7729f09068df9371748b99697bf20ea347b3d7
Maik Forte <michael_ardan2000@yahoo.com>
2025-03-08 21:50:32 +0800

    Initial Setup

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,35 +1,37 @@
 /** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 const config = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'es5',
-  plugins: ['@ianvs/prettier-plugin-sort-imports'],
-  importOrder: [
-    '.*styles.css$',
-    '',
-    'dayjs',
-    '^react$',
-    '^next$',
-    '^next/.*$',
-    '<BUILTIN_MODULES>',
-    '<THIRD_PARTY_MODULES>',
-    '^@mantine/(.*)$',
-    '^@mantinex/(.*)$',
-    '^@mantine-tests/(.*)$',
-    '^@docs/(.*)$',
-    '^@/.*$',
-    '^../(?!.*.css$).*$',
-    '^./(?!.*.css$).*$',
-    '\\.css$',
-  ],
-  overrides: [
-    {
-      files: '*.mdx',
-      options: {
-        printWidth: 70,
-      },
-    },
-  ],
+	printWidth: 100,
+	singleQuote: false,
+	tabWidth: 4,
+	useTabs: true,
+	trailingComma: "es5",
+	plugins: ["@ianvs/prettier-plugin-sort-imports"],
+	importOrder: [
+		".*styles.css$",
+		"",
+		"dayjs",
+		"^react$",
+		"^next$",
+		"^next/.*$",
+		"<BUILTIN_MODULES>",
+		"<THIRD_PARTY_MODULES>",
+		"^@mantine/(.*)$",
+		"^@mantinex/(.*)$",
+		"^@mantine-tests/(.*)$",
+		"^@docs/(.*)$",
+		"^@/.*$",
+		"^../(?!.*.css$).*$",
+		"^./(?!.*.css$).*$",
+		"\\.css$",
+	],
+	overrides: [
+		{
+			files: "*.mdx",
+			options: {
+				printWidth: 70,
+			},
+		},
+	],
 };
 
 export default config;
